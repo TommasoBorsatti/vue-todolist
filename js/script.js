6207873e7767db9fb039971c02372f4c59c97467
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-26 13:50:30 +0100

    File Pronti - Script degli elementi lista pronto

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+const App = new Vue (
+
+  {
+    el: '#root',
+
+    data: {
+
+      list:[
+        "Svegliarsi alle 8:30",
+        "Connettersi alla classe Boolean alle 9:30",
+        "Pausella caffè 11:10",
+        "Pranzo 13:15",
+        "Esercizio sulla To Do list con Vue --> Right Now!" 
+      ],
+
+
+
+    },
+
+    methods: {
+
+    },
+
+  }
+
+);
